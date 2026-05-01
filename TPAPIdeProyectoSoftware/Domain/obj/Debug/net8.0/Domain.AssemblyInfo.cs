8f9eda69f31ca84ccbb7089b603d15dc1bf42ace
NahuelCoronel28 <nagucoronel@gmail.com>
2026-05-01 15:21:14 -0300

    correciones del front de pagos

--- TPAPIdeProyectoSoftware/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ TPAPIdeProyectoSoftware/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5af842e4c077925e66e558e5eef62c885867ef8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2727391da78aea4c05c0187f6091e63788bdbb06")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
