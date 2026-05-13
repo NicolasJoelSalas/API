1f8a37a35674773df8a3b6ad6f3d9c8347d8a250
NahuelCoronel28 <nagucoronel@gmail.com>
2026-05-12 22:01:35 -0300

    Front de reserva expirada

--- TPAPIdeProyectoSoftware/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ TPAPIdeProyectoSoftware/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b08d56bf8c62a82e69d291dbd73a92049fd9c6f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e814864d20d6b3fae3690d9911be48b91f10569c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TPAPIdeProyectoSoftware/TPAPIdeProyectoSoftware/obj/Debug/net8.0/TPAPIdeProyectoSoftware.AssemblyInfo.cs
+++ TPAPIdeProyectoSoftware/TPAPIdeProyectoSoftware/obj/Debug/net8.0/TPAPIdeProyectoSoftware.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TPAPIdeProyectoSoftware")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b08d56bf8c62a82e69d291dbd73a92049fd9c6f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e814864d20d6b3fae3690d9911be48b91f10569c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TPAPIdeProyectoSoftware")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TPAPIdeProyectoSoftware")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
