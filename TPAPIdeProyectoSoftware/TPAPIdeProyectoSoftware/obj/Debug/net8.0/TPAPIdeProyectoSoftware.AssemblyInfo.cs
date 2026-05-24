088c4733a5e97a0b3c3176fccc28c3efa95c3520
NahuelCoronel28 <nagucoronel@gmail.com>
2026-05-24 11:25:00 -0300

    coreccion menor del front

--- TPAPIdeProyectoSoftware/TPAPIdeProyectoSoftware/obj/Debug/net8.0/TPAPIdeProyectoSoftware.AssemblyInfo.cs
+++ TPAPIdeProyectoSoftware/TPAPIdeProyectoSoftware/obj/Debug/net8.0/TPAPIdeProyectoSoftware.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TPAPIdeProyectoSoftware")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d2562ae69e31d294c25046a13fe57cfab34d9ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96685da3a2451a1be8e0914f495c94fb1e0be5c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TPAPIdeProyectoSoftware")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TPAPIdeProyectoSoftware")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
