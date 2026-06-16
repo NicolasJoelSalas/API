06c4c3f7c52ff6d593be4371906b9030d30cc148
NahuelCoronel28 <nagucoronel@gmail.com>
2026-06-15 23:18:04 -0300

    Eliminacion del front de la solucion del back

--- TPAPIdeProyectoSoftware/TPAPIdeProyectoSoftware/obj/Debug/net8.0/TPAPIdeProyectoSoftware.AssemblyInfo.cs
+++ TPAPIdeProyectoSoftware/TPAPIdeProyectoSoftware/obj/Debug/net8.0/TPAPIdeProyectoSoftware.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TPAPIdeProyectoSoftware")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02a9d1cb212595a13f7d4b4b893b2cb18efee346")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3eaa6a6d05fca3bea0df7b88bbc7d62680435a8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TPAPIdeProyectoSoftware")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TPAPIdeProyectoSoftware")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
