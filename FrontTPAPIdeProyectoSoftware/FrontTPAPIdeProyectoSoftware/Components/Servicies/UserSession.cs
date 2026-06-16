3eaa6a6d05fca3bea0df7b88bbc7d62680435a8c
NahuelCoronel28 <nagucoronel@gmail.com>
2026-06-15 23:16:55 -0300

    Separacion de Front

---
+++ FrontTPAPIdeProyectoSoftware/FrontTPAPIdeProyectoSoftware/Components/Servicies/UserSession.cs
@@ -0,0 +1,10 @@
+﻿namespace FrontTPAPIdeProyectoSoftware.Components.Servicies
+{
+    public class UserSession
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+
+        public bool IsLogged => UserId > 0;
+    }
+}
